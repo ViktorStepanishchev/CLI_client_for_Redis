78d16dd1c8468974062e407cb53ca109bb5b8f9a
byvs <v.stepanishchev@bk.ru>
2024-05-15 20:00:20 +0300

    main commands, no lists/hash/SSL/logging

---
+++ my_redis_cli.cpp
@@ -0,0 +1,212 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <string.h>
+#include <ctype.h>
+using namespace std;
+
+#define SERVER_IP "127.0.0.1"
+#define DEFAULT_PORT 6379
+#define BUFFER_SIZE 1024
+
+std::vector<std::string> break_text_to_words(const std::string& msg) {
+    std::vector<std::string> words;
+    std::istringstream potok(msg);
+    std::string word;
+    while (potok >> word) {
+        words.push_back(word);
+    }
+    return words;
+}
+
+std::string create_RESP_command(const std::string& command, const std::string& parametr, const std::string& value, const std::string& sec_value, int att) {
+    if (att == 4){
+        std::stringstream resp;
+        resp << "*" << att << "\r\n";
+        resp << "$" << command.size() << "\r\n";
+        resp << command << "\r\n";
+        resp << "$" << parametr.size() << "\r\n";
+        resp << parametr << "\r\n";
+        resp << "$" << value.size() << "\r\n";
+        resp << value << "\r\n";
+        resp << "$" << sec_value.size() << "\r\n";
+        resp << sec_value << "\r\n";
+        return resp.str();
+    }
+    if (att == 3){
+        std::stringstream resp;
+        resp << "*" << att << "\r\n";
+        resp << "$" << command.size() << "\r\n";
+        resp << command << "\r\n";
+        resp << "$" << parametr.size() << "\r\n";
+        resp << parametr << "\r\n";
+        resp << "$" << value.size() << "\r\n";
+        resp << value << "\r\n";
+        return resp.str();
+    }
+    if (att == 2){
+        std::stringstream resp;
+        resp << "*" << att << "\r\n";
+        resp << "$" << command.size() << "\r\n";
+        resp << command << "\r\n";
+        resp << "$" << parametr.size() << "\r\n";
+        resp << parametr << "\r\n";
+        return resp.str();
+    }
+    if (att == 1){
+        std::stringstream resp;
+        resp << "*" << att << "\r\n";
+        resp << "$" << command.size() << "\r\n";
+        resp << command << "\r\n";
+        return resp.str();
+    }
+}
+
+std::string commands_with_any_attribute(std::string msg, int sock, char *buffer){
+    std::vector<std::string>words_msg = break_text_to_words(msg);
+    if (words_msg[0] == "SET"){
+        std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 3);
+        send(sock, command.c_str(), command.size(), 0);
+        read(sock, buffer, BUFFER_SIZE);
+        return buffer;
+    }
+    if (words_msg[0] == "GET"){
+        std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 2);
+        send(sock, command.c_str(), command.size(), 0);
+        read(sock, buffer, BUFFER_SIZE);
+        return buffer;
+    }
+    if (words_msg[0] == "DEL"){
+        std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 2);
+        send(sock, command.c_str(), command.size(), 0);
+        read(sock, buffer, BUFFER_SIZE);
+        return buffer;
+    }
+    if (words_msg[0] == "TYPE"){
+        std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 2);
+        send(sock, command.c_str(), command.size(), 0);
+        read(sock, buffer, BUFFER_SIZE);
+        return buffer;
+    }
+    if (words_msg[0] == "ECHO"){
+        std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 2);
+        send(sock, command.c_str(), command.size(), 0);
+        read(sock, buffer, BUFFER_SIZE);
+        return buffer;
+    }
+    if (words_msg[0] == "KEYS"){
+        std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 2);
+        send(sock, command.c_str(), command.size(), 0);
+        read(sock, buffer, BUFFER_SIZE);
+        return buffer;
+    }
+    if (words_msg[0] == "UNLINK"){
+        std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 2);
+        send(sock, command.c_str(), command.size(), 0);
+        read(sock, buffer, BUFFER_SIZE);
+        return buffer;
+    }
+    if (words_msg[0] == "EXPIRE"){
+        std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 3);
+        send(sock, command.c_str(), command.size(), 0);
+        read(sock, buffer, BUFFER_SIZE);
+        return buffer;
+    }
+    if (words_msg[0] == "RENAME"){
+        std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 3);
+        send(sock, command.c_str(), command.size(), 0);
+        read(sock, buffer, BUFFER_SIZE);
+        return buffer;
+    }
+    if (words_msg[0] == "LPUSH"){
+        std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 3);
+        send(sock, command.c_str(), command.size(), 0);
+        read(sock, buffer, BUFFER_SIZE);
+        return buffer;
+    }
+    if (words_msg[0] == "RPUSH"){
+        std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 3);
+        send(sock, command.c_str(), command.size(), 0);
+        read(sock, buffer, BUFFER_SIZE);
+        return buffer;
+    }
+    return "Error command!\n";
+}
+
+std::string command_no_space(std::string msg, int sock, char* buffer){
+    if (msg == "ping"){
+        std::string command = create_RESP_command(msg, "", "", "", 1);;
+        send(sock, command.c_str(), command.size(), 0);
+        read(sock, buffer, BUFFER_SIZE);
+        return buffer;
+    }
+    if (msg == "info"){
+        std::string command = create_RESP_command(msg, "", "", "", 1);
+        send(sock, command.c_str(), command.size(), 0);
+        read(sock, buffer, BUFFER_SIZE);
+        return buffer;
+    }
+    if (msg == "flushall"){
+        std::string command = create_RESP_command(msg, "", "", "", 1);
+        send(sock, command.c_str(), command.size(), 0);
+        read(sock, buffer, BUFFER_SIZE);
+        return buffer;
+    }
+    return "Error command!\n";
+}
+
+bool quit(std::string msg){
+    if (msg == "quit"){
+        return true;
+    }
+}
+
+int main() {
+    int sock, client;
+    struct sockaddr_in serv_addr;
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        cout << "Ошибка создания сокета" << std::endl;
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(DEFAULT_PORT);
+
+    if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
+        cout << "Неверный адрес или адрес не поддерживается" << std::endl;
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        cout << "Ошибка подключения" << std::endl;
+        return -1;
+    }
+
+
+    std::string message;
+    char buffer[BUFFER_SIZE] = {0};
+    std::string command;
+
+    while (true) {
+        cout << SERVER_IP << ":" << DEFAULT_PORT << ">";
+        getline(cin, message);
+        if (quit(message)) {
+            command = "*1\\r\\n$4\\r\\nQUIT\\r\\n";
+            cout << "Byebye...\n";
+            send(sock, command.c_str(), command.size(), 0);
+            break;
+        }
+        if (message.find(' ') > 0 && message.find(' ') != 18446744073709551615) {
+            cout << commands_with_any_attribute(message, sock, buffer);
+        }
+        if (message.find(' ') == 18446744073709551615) {
+            cout << command_no_space(message, sock, buffer);
+        }
+    }
+    close(client);
+    return 0;
+}
