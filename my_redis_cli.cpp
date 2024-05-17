b900b6530c9c56b0c2c875cf0b5ff1d615c79c14
byvs <v.stepanishchev@bk.ru>
2024-05-17 21:10:54 +0300

    Fix bugs with read from server

--- my_redis_cli.cpp
+++ my_redis_cli.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <vector>
@@ -9,13 +8,11 @@
 #include <string.h>
 #include <ctype.h>
 #include <typeinfo>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
 using namespace std;
 
-#define BUFFER_SIZE 1024
-#define REDIS_SERVER "127.0.0.1"
-#define REDIS_PORT 6379
+#define SERVER_IP "127.0.0.1"
+#define DEFAULT_PORT 6379
+#define BUFFER_SIZE 4096
 
 std::vector<std::string> break_text_to_words(const std::string& msg) {
     std::vector<std::string> words;
@@ -149,47 +146,63 @@ std::string create_RESP_command(const std::string& command, const std::string& p
 
 std::string commands_with_any_attribute(std::string msg, int sock, char *buffer){
     std::vector<std::string>words_msg = break_text_to_words(msg);
+    if (msg == "KEYS *"){
+        std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 2);
+        send(sock, command.c_str(), command.size(), 0);
+        read(sock, buffer, BUFFER_SIZE);
+        return buffer;
+    }
     if (words_msg[0] == "SET"){
         std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 3);
         send(sock, command.c_str(), command.size(), 0);
         read(sock, buffer, BUFFER_SIZE);
-        return buffer;
+        std::vector res = break_text_to_words(buffer);
+        return res[0];
     }
     if (words_msg[0] == "GET"){
         std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 2);
         send(sock, command.c_str(), command.size(), 0);
         read(sock, buffer, BUFFER_SIZE);
-        return buffer;
+        std::vector res = break_text_to_words(buffer);
+        if (res.size() == 1){
+            return res[0];
+        }
+        return res[1];
     }
     if (words_msg[0] == "DEL"){
         std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 2);
         send(sock, command.c_str(), command.size(), 0);
         read(sock, buffer, BUFFER_SIZE);
-        return buffer;
+        std::vector res = break_text_to_words(buffer);
+        return res[0];
     }
     if (words_msg[0] == "TYPE"){
         std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 2);
         send(sock, command.c_str(), command.size(), 0);
         read(sock, buffer, BUFFER_SIZE);
-        return buffer;
+        std::vector res = break_text_to_words(buffer);
+        return res[0];
     }
     if (words_msg[0] == "ECHO"){
         std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 2);
         send(sock, command.c_str(), command.size(), 0);
         read(sock, buffer, BUFFER_SIZE);
-        return buffer;
+        std::vector res = break_text_to_words(buffer);
+        return res[1];
     }
     if (words_msg[0] == "KEYS"){
         std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 2);
         send(sock, command.c_str(), command.size(), 0);
         read(sock, buffer, BUFFER_SIZE);
-        return buffer;
+        std::vector res = break_text_to_words(buffer);
+        return res[2];
     }
     if (words_msg[0] == "UNLINK"){
-        std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 2);
+        std::string command = create_RESP_command_for_hget(words_msg);
         send(sock, command.c_str(), command.size(), 0);
         read(sock, buffer, BUFFER_SIZE);
-        return buffer;
+        std::vector res = break_text_to_words(buffer);
+        return res[0];
     }
     if (words_msg[0] == "EXPIRE"){
         std::string command = create_RESP_command(words_msg[0], words_msg[1], words_msg[2], words_msg[3], 3);
@@ -328,10 +341,11 @@ std::string commands_with_any_attribute(std::string msg, int sock, char *buffer)
 
 std::string command_no_space(std::string msg, int sock, char* buffer){
     if (msg == "ping"){
-        std::string command = create_RESP_command(msg, "", "", "", 1);;
+        std::string command = create_RESP_command(msg, "", "", "", 1);
         send(sock, command.c_str(), command.size(), 0);
         read(sock, buffer, BUFFER_SIZE);
-        return buffer;
+        std::vector res = break_text_to_words(buffer);
+        return res[0];
     }
     if (msg == "info"){
         std::string command = create_RESP_command(msg, "", "", "", 1);
@@ -343,7 +357,8 @@ std::string command_no_space(std::string msg, int sock, char* buffer){
         std::string command = create_RESP_command(msg, "", "", "", 1);
         send(sock, command.c_str(), command.size(), 0);
         read(sock, buffer, BUFFER_SIZE);
-        return buffer;
+        std::vector res = break_text_to_words(buffer);
+        return res[0];
     }
     return "Error command!\n";
 }
@@ -355,54 +370,46 @@ bool quit(std::string msg){
 }
 
 int main() {
-    SSL_library_init();
-    SSL_load_error_strings();
-    OpenSSL_add_all_algorithms();
-
-    SSL_CTX *ctx = SSL_CTX_new(SSLv23_client_method());
-    SSL *ssl = SSL_new(ctx);
-
-    int server = socket(AF_INET, SOCK_STREAM, 0);
-    sockaddr_in sa_serv;
-    memset(&sa_serv, '\0', sizeof(sa_serv));
-
-    sa_serv.sin_family = AF_INET;
-    sa_serv.sin_port = htons(REDIS_PORT);
-    sa_serv.sin_addr.s_addr = inet_addr(REDIS_SERVER);
-
-    connect(server, (struct sockaddr*) &sa_serv, sizeof(sa_serv));
-
-    SSL_set_fd(ssl, server);
-
-    if (SSL_connect(ssl) == -1) {
-        ERR_print_errors_fp(stderr);
-    } else {
-        printf("Connected with %s encryption\n", SSL_get_cipher(ssl));
-        // Здесь вы можете написать код для отправки и получения данных от сервера Redis.
+    int sock;
+    struct sockaddr_in serv_addr;
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        cout << "Ошибка создания сокета" << std::endl;
+        return -1;
     }
 
-    SSL_shutdown(ssl);
-    SSL_free(ssl);
-    close(server);
-    SSL_CTX_free(ctx);
-    EVP_cleanup();
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(DEFAULT_PORT);
 
+    if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
+        cout << "Неверный адрес или адрес не поддерживается" << std::endl;
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        cout << "Ошибка подключения" << std::endl;
+        return -1;
+    }
+
+
+    std::string message;
+    char buffer[BUFFER_SIZE] = {0};
+    std::string command;
+
+    while (true) {
+        cout << SERVER_IP << ":" << DEFAULT_PORT << ">";
+        getline(cin, message);
+        if (quit(message)) {
+            break;
+        }
+        if (message.find(' ') > 0 && message.find(' ') != 18446744073709551615) {
+            cout << commands_with_any_attribute(message, sock, buffer) << "\n";
+        }
+        if (message.find(' ') == 18446744073709551615) {
+            cout << command_no_space(message, sock, buffer) << "\n";
+        }
+    }
+    close(sock);
     return 0;
 }
 
 
-//while (true) {
-//        cout << SERVER_IP << ":" << DEFAULT_PORT << ">";
-//        getline(cin, message);
-//        if (quit(message)) {
-//            command = "*1\\r\\n$4\\r\\nQUIT\\r\\n";
-//            send(sock, command.c_str(), command.size(), 0);
-//            break;
-//        }
-//        if (message.find(' ') > 0 && message.find(' ') != 18446744073709551615) {
-//            cout << commands_with_any_attribute(message, sock, buffer);
-//        }
-//        if (message.find(' ') == 18446744073709551615) {
-//            cout << command_no_space(message, sock, buffer);
-//        }
-//    }
